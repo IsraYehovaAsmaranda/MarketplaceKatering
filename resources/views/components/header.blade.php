496acc6eb8689e0b1b16ae008090841edc14da76
IsraYehovaAsmaranda <israasmaranda@gmail.com>
2024-09-06 03:53:12 +0700

    menambahkan search button

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,13 +3,18 @@
         <div class="text-xl font-bold hidden md:block">
             Marketplace Katering
         </div>
-        <div class="flex-1 mx-4">
+        <div class="flex-1 mx-4 flex items-center space-x-2">
             <input type="text" placeholder="Cari Makanan..."
                 class="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500">
+            <button type="button"
+                class="py-2 px-4 bg-blue-700 text-white rounded-md hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
         </div>
         @if (Auth::check())
             <div class="flex items-center space-x-4 relative">
-                <a type="button" href="/account" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50">Account</a>
+                <a type="button" href="/account"
+                    class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50">Account</a>
             </div>
         @else
             <div class="flex items-center space-x-4">
@@ -25,34 +30,3 @@
         @endif
     </div>
 </header>
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const button = document.getElementById('dropdown-button');
-        const menu = document.getElementById('dropdown-menu');
-
-        if (!button || !menu) {
-            console.error('Button or Menu element not found.');
-            return;
-        }
-
-        // Create Popper instance
-        const popperInstance = Popper.createPopper(button, menu, {
-            placement: 'bottom-end',
-        });
-
-        // Toggle menu visibility on button click
-        button.addEventListener('click', function() {
-            menu.classList.toggle('hidden');
-            popperInstance.update(); // Update Popper position if needed
-            console.log('Button clicked, toggling menu visibility.');
-        });
-
-        // Hide menu if click outside
-        document.addEventListener('click', function(event) {
-            if (!button.contains(event.target) && !menu.contains(event.target)) {
-                menu.classList.add('hidden');
-                console.log('Click outside detected, hiding menu.');
-            }
-        });
-    });
-</script>
